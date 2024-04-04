db56b510bce804e00d6d47c4a71d67e668816198
taqiyyah19 <146053018+taqiyyah19@users.noreply.github.com>
2024-04-04 22:10:47 +0700

    Add files via upload

---
+++ model.php
@@ -0,0 +1,23 @@
+<?php
+class contacts {
+    static function select() {
+
+    }
+    
+    static function insert() {
+      
+    }
+    
+    static function update() {
+        
+    }
+    
+    static function delete() {
+        
+    }
+}
+
+echo contacts::select() . "\n";
+echo contacts::insert() . "\n";
+echo contacts::update() . "\n";
+echo contacts::delete() . "\n";
