2b30f65722dd8961cfb3981f3ad46e4fb213efc0
taqiyyah19 <146053018+taqiyyah19@users.noreply.github.com>
2024-05-24 21:10:37 +0700

    Update AuthController.php

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -47,18 +47,3 @@ class AuthController {
         }
     }
 
-    static function logout() {
-        $_SESSION = array();
-
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params["path"], $params["domain"],
-                $params["secure"], $params["httponly"]
-            );
-        }
-
-        session_destroy();
-        header('Location: '.BASEURL. 'login');
-    }
-}
